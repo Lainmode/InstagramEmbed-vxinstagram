fc774ca7ce5aa5abb344c8025c572218e4f8fc55
Lainmode <mali71759@gmail.com>
2025-11-27 16:20:42 +0100

    removed blazor, added base for reactjs project

---
+++ InstagramEmbed.Web/InstagramEmbed.Web.Server/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InstagramEmbed.Web.Server.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
