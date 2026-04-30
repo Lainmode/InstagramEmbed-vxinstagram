5cea9f09d2fb0dd2c059022c627475c5a892a35a
Lainmode <mali71759@gmail.com>
2026-04-30 19:18:02 +0200

    added donation settings and env variables

---
+++ InstagramEmbedForDiscord/Services/DonationSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace InstagramEmbed.Application.Services
+{
+    public class DonationSettings
+    {
+        public string Password { get; set; } = "password";
+        public int Current { get; set; } = 0;
+        public int Target { get; set; } = 100;
+    }
+}
